542d446dec38d9f6a046c7f335c61c0b3c76d1c7
batool <batoolsh2001@gmail.com>
2022-07-20 13:21:28 +0300

    project

---
+++ js/explor.js
@@ -0,0 +1,15 @@
+
+const menuItems = document.querySelectorAll(".menu-item");
+
+const  changeActiveItem =() => {
+    menuItems.forEach(item => {
+        item.classList.remove('active');
+    })
+}
+
+menuItems.forEach(item => {
+    item.addEventListener('click', () => {
+        changeActiveItem();
+        item.classList.add('active');
+    })
+})
